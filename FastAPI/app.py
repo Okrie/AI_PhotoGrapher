c98eba7b7ce3363462e06c00bb83b3ee6b5799cf
Okrie <mykkang55@naver.com>
2023-10-27 03:37:20 +0900

    pred module 추가

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -4,6 +4,8 @@ from fastapi.templating import Jinja2Templates
 from typing import Union
 import os
 import pymysql
+from .static.pred import pred
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -11,6 +13,13 @@ templates = Jinja2Templates(directory="templates")
 @app.get('/')
 def read_root(request: Request):
     return templates.TemplateResponse('index.html', {'request' : request})
+
+
+@app.get('/pred')
+def pred_image(request: Request):
+
+    return pred()
+    
 
 # import subprocess
 

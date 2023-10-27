d8480ed11464215a7e62ed779a3cbaf22ef74a71
Okrie <mykkang55@naver.com>
2023-10-27 22:20:51 +0900

    filter 뷰에서 이미지 받기 성공

--- Flutter/photographer_ai/lib/View/filter.dart
+++ Flutter/photographer_ai/lib/View/filter.dart
@@ -1,11 +1,53 @@
 import 'dart:io';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:typed_data';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:photographer_ai/Model/aiImage/ai_image.dart';
 import 'package:photographer_ai/ViewModel/filter/filter_vm.dart';
+
+
+// 단일 이미지 받아옴 테스트
+Future<String> _fetchImage() async {
+  final response = await http.get(Uri.parse('http://121.130.179.94:8000/pred'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> list = jsonDecode(response.body);
+    Map<String, dynamic> result = jsonDecode(list[0]['result']); // 리스트의 첫 번째 요소에 접근
+    String imageBase64 = result['image'];
+    return imageBase64;
+  } else {
+    throw Exception('Failed to load image');
+  }
+}
+
+// 여러개의 생성된 이미지를 받아온다.
+Future<List<String>> _fetchImages() async {
+  final response = await http.get(Uri.parse('http://121.130.179.94:8000/pred'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> list = jsonDecode(response.body);
+    List<String> images = [];
+
+    for (var item in list) {
+      if (item is Map) {
+        List<dynamic> results = item['result']; // 'result' 키의 값이 리스트
+        for (var res in results) {
+          Map<String, dynamic> resultJson = jsonDecode(res); // 리스트의 각 요소는 문자열로 된 JSON
+          String imageBase64 = resultJson['image']; // JSON에서 'image' 키의 값을 가져옴
+          images.add(imageBase64);
+        }
+      }
+    }
+
+    return images;
+  } else {
+    throw Exception('Failed to load images');
+  }
+}
+
 
 class PhotoGrapherFilter extends StatelessWidget{
   const PhotoGrapherFilter({super.key});
@@ -32,191 +74,57 @@ class PhotoGrapherFilter extends StatelessWidget{
           )
         ],
       ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 150,
-              width: MediaQuery.of(context).size.width,
-              child: Obx(() => fcontroller.image.isEmpty
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.image_not_supported_outlined, size: 85, color: Colors.grey[400]),
-                    ],
-                  )
-                : Image.file(File(fcontroller.image.value)),
-              )
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 50)
-            ),
-            const Text('AI Recommend Filter'),
-            Obx(() => fcontroller.image.value.isEmpty
+      body: Column(
+        children: [
+          SizedBox(
+            height: 150,
+            width: MediaQuery.of(context).size.width,
+            child: Obx(() => fcontroller.image.isEmpty
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.image_not_supported_outlined, size: 85, color: Colors.grey[400]),
+                  ],
+                )
+              : Image.file(File(fcontroller.image.value)),
+            )
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 50)
+          ),
+          const Text('AI Recommend Filter'),
+          SingleChildScrollView(
+            child: Obx(() => fcontroller.image.value.isEmpty
               ? Padding(
                   padding: const EdgeInsets.only(top: 50),
                   child:
-                      Text('사진을 찍거나\n아래 버튼을 통해 사진을 올려주세요.', textAlign: TextAlign.center),
+                      Text('사진을 찍거나\n상단 버튼을 통해 사진을 올려주세요.', textAlign: TextAlign.center),
                 )
-              : Center(
-              child: fcontroller.image.value.isNotEmpty
-                ? Container(
-                  height: MediaQuery.of(context).size.height * 0.4,
-                  child: ListView.builder(
-                    itemCount: fcontroller.aiImageList.length, // 필터 개수에 맞게 변경해야 함
-                    itemBuilder: (context, index) {
-                      final AiImage aiImage = fcontroller.aiImageList[index]; // 필터 정보를 가져와서 사용해야 함
-
-                      return GestureDetector(
-                        onTap: () {
-                          // 필터 선택 시 동작 구현
-                        },
-                        child: Card(
-                          // 웹에서 정보 받아와서 뿌려줄 것
-                          // getJSONData();
-                          // aiImageList[index].data;
-                          child: Text(aiImage.toString()), // 예시로 필터 이름을 표시함
-                        ),
-                      );
-                    },
-                  ),
-                )
-                : const Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: CircularProgressIndicator(),
-                ),
+              : FutureBuilder<List<String>>(
+                future: _fetchImages(),
+                builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator(); // 로딩 중일 때는 로딩 인디케이터 표시
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}'); // 오류 발생 시 오류 메시지 표시
+                  } else {
+                    return ListView.builder( // 이미지 데이터 로드 완료 시 리스트뷰로 표시
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        String imageBase64 = snapshot.data![index];
+                        Uint8List bytes = base64Decode(imageBase64);
+                    
+                        return Image.memory(bytes); // base64 인코딩된 이미지 데이터를 이미지로 변환하여 표시
+                      },
+                    );
+                  }
+                },
               )
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
 }
-
-// class PhotoGrapherFilter extends StatefulWidget {
-//   const PhotoGrapherFilter({super.key});
-
-//   @override
-//   State<PhotoGrapherFilter> createState() => _PhotoGrapherFilterState();
-// }
-
-// class _PhotoGrapherFilterState extends State<PhotoGrapherFilter> {
-
-//   late List<AiImage> aiImageList;
-//   XFile? _image;
-//   final ImagePicker picker = ImagePicker();
-//   var value = Get.arguments ?? '__';
-//   bool isLoaded = false;
-//   bool imgLoad = false;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     aiImageList = [];
-//     isLoaded = value != '__' ? false : true;
-//   }
-  
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('AI PhotoGrapher'),
-//         centerTitle: true,
-//         actions: [
-//           IconButton(
-//             onPressed: () {
-//               getImage(ImageSource.gallery).then((value) => setState(() {
-//                 if(_image != null){
-//                   print(_image!.path);
-//                   imgLoad = true;
-//                   print(imgLoad);
-//                 }
-//               }));
-//             },
-//             icon: const Icon(Icons.folder, color: Colors.amber),
-//           )
-//         ],
-//       ),
-//       body: Center(
-//         child: Column(
-//           children: [
-//             SizedBox(
-//               height: 150,
-//               width: MediaQuery.of(context).size.width,
-//               child: value == '__' || !imgLoad ?
-//               Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   Icon(Icons.image_not_supported_outlined, size: 85, color: Colors.grey[400],),
-//                 ],
-//               )
-//               : Image.file(
-//                 File(!imgLoad ? value[1] : _image!.path),
-//               ),
-//             ),
-//             const Text('AI Recommend Filter'),
-//             value == '__' || _image == null
-//             ? Padding(
-//               padding: const EdgeInsets.only(top: 50),
-//               child: Text('사진을 찍거나\n아래 버튼을 통해 사진을 올려주세요.', textAlign: TextAlign.center)
-//             )
-//             : Center(
-//               child: isLoaded
-//                 ? Container(
-//                   height: MediaQuery.of(context).size.height*0.5,
-//                   child: ListView.builder(
-//                     itemBuilder: (context, index) {
-//                       return GestureDetector(
-//                         onTap: () {
-//                           //
-//                         },
-//                         child: Card(
-//                           // 웹에서 정보 받아와서 뿌려줄것
-//                           // getJSONData();
-//                           // aiImageList[index].data;
-//                         ),
-//                       );
-//                     },
-//                   )
-//                 )
-//                 : const Padding(
-//                   padding: EdgeInsets.only(top: 20),
-//                   child: CircularProgressIndicator(),
-//                 ),
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   getJSONData() async {
-//     var url = Uri.parse(
-//         'http://localhost:8080/Flutter/dup_check_select_flutter.jsp?userid=${0}');
-//     var response = await http.get(url);
-//     // data.clear();
-//     var dataConvertedJSON = json.decode(utf8.decode(response.bodyBytes));
-//     String result = dataConvertedJSON['result'];
-//     setState(() {});
-//     print(result);
-//     if (result == 'success') {
-//       // aiImageList.add(value);
-//     } else {
-//       // 
-//     }
-//   }
-
-  // //이미지를 가져오는 함수
-  // Future getImage(ImageSource imageSource) async {
-  //   //pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
-  //   final XFile? pickedFile = await picker.pickImage(source: imageSource);
-  //   if (pickedFile != null) {
-  //     setState(() {
-  //       _image = XFile(pickedFile.path); //가져온 이미지를 _image에 저장
-  //     });
-  //   } else{
-  //     _image = null;
-  //   }
-  // }
-// }

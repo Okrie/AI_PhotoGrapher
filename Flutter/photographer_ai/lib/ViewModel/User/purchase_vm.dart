7865719c1c6ad4d38f3e39b917b1c29464024737
Okrie <mykkang55@naver.com>
2023-10-30 18:20:19 +0900

    Flutter - 구매정보 관련 GetX

---
+++ Flutter/photographer_ai/lib/ViewModel/User/purchase_vm.dart
@@ -0,0 +1,45 @@
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class PurchaseController{
+  RxBool isLoad = false.obs;
+  RxInt seq = 99.obs;
+  RxBool isView = false.obs;
+  RxList<Map<String, dynamic>> data = <Map<String, dynamic>>[].obs;
+
+
+  // 필터 조회 및 구매
+  Future<List<Map<String, dynamic>>> fetchFilter(userid) async {
+    if (!isLoad.value){
+      if (isView.value){
+        var url = Uri.parse('http://flask.okrie.kro.kr:8000/userinfo?userid=${userid}');
+        var response = await http.get(url);
+        if (response.statusCode == 201) {
+          List<dynamic> list = jsonDecode(response.body)[0]['result'];
+          for (var item in list) {
+            Map<String, dynamic> resultJson = item;
+            data.add(resultJson);
+          }
+          isLoad.value = true;
+          return data;
+        } else {
+          throw Exception('Failed to connect Server');
+        }
+      } else {
+        return data;
+      }
+    }
+    return data;
+  }
+
+  // Nickname 가져오기
+  String getNicknameBySeq(List<Map<String, dynamic>> data, int seq) {
+    for (var item in data) {
+      if (item['seq'] == seq) {
+        return item['pnickname'];
+      }
+    }
+    return ''; // 해당 seq를 가진 아이템이 없을 경우 빈 문자열 반환 또는 원하는 예외 처리 추가
+  }
+}
